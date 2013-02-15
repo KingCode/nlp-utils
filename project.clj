612dc374d4f92269bdb03f384837fab2bfbe4067
KingCode <jrompre@gmail.com>
2013-02-15 18:33:42 -0500

    project dependencies, README update, previously omitted

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.apache.opennlp/opennlp-tools "1.5.2-incubating"]
+                 [commons-io/commons-io "2.4"]
+                ]
+  :resource-paths [ "resources/opennlp/models" ]
+  :repositories ["snapshots" "https://repository.apache.org/content/repositories/snapshots"]
+  :jvm-opts [ "-Xmx1024m"]
+)
