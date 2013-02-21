62d8a8d69cf9ffb9b4362119044a4642ad624466
KingCode <jrompre@gmail.com>
2013-02-21 07:37:36 -0500

    Previously omitted file due to renaming

---
+++ test/nlp_utils/opennlp_core_test.clj
@@ -0,0 +1,49 @@
+ (ns nlp-utils.opennlp-core-test
+  (:use clojure.test
+        nlp-utils.opennlp-core))
+
+(deftest sentences-of-test
+  (testing "Detect the correct number of sentences and reproduce their content"
+    (let [ text "This is the first sentence. Fido went to drink water out of the toilet." 
+           sentences (sentences-of text) ] 
+        (is (= 2 (count sentences))))))
+
+(deftest parse-line-test
+  (testing "Parse a line (sentence) and output the grammatical structure"
+    (let [ text (str-from-file "test/data/financial-lnkd.txt")
+           sentences (sentences-of text)
+           lines (take 1 (drop 2 sentences))
+           line (nth lines 0)
+           model (chunking-parser-model)
+           p (parser model)
+           results (parse-line p line) 
+        ]
+        (is (not (empty? results)))
+     )))
+
+(deftest show-parse-test
+  (testing "Show a parse result in  Penn Treebank format"
+    (let [ text (str-from-file "test/data/financial-lnkd.txt")
+           sentences (sentences-of text)
+           lines (take 1 (drop 5 sentences))
+           line (first lines)
+           model (chunking-parser-model)
+           p (parser model)
+           pres (parse-line p line 5)
+           input-1 (nth pres 0)
+           input-2 (nth pres 1)
+           input-3 (take 2 pres) 
+           r-1 (show-parse input-1)
+           r-2 (show-parse input-2)
+           r-3 (show-parse input-3)
+           r-4 (show-parse input-3 true "LINE")
+         ]
+
+   (println "SHOW=PARSE>\n" r-1) 
+   (println "SHOW=PARSE>\n" r-2) 
+   (println "SHOW=PARSE>\n" r-3)
+   (println "SHOW-PARSE>\n" r-4) ))) 
+
+
+
+
