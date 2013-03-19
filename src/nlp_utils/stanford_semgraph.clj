a1e83ce07c5c70881dd72eeb0d78efbdd24182ff
KingCode <jrompre@gmail.com>
2013-03-19 19:22:23 -0400

    stanford-semgraph: ner extraction and queries

--- src/nlp_utils/stanford_semgraph.clj
+++ src/nlp_utils/stanford_semgraph.clj
@@ -1,5 +1,5 @@
 (ns nlp-utils.stanford-semgraph
-
+(:import (edu.stanford.nlp.ling CoreAnnotations CoreAnnotations$NormalizedNamedEntityTagAnnotation))
 )
 
 (defn get-edges
@@ -14,14 +14,46 @@ if incoming? is true, or as governor otherwise.
 
 (defn get-out-edges
 "Yields the graph's edges bearing a relation matching reln-re and  having node as the governor." 
-[ graph node reln-re ]
+([ graph node reln-re ]
   (get-edges graph node reln-re false))
+([ graph node ]
+  (get-out-edges graph node ".*")))
 
 
 (defn get-in-edges
 "Yields the graph's edges bearing a relation matching reln-re and  having node as the dependent." 
-[ graph node reln-re ]
+([ graph node reln-re ]
   (get-edges graph node reln-re true))
+([ graph node ]
+  (get-in-edges graph node ".*")))
 
 
+(defn money?
+[ node ]
+  (= "MONEY" (.ner node)))
+
+
+(defn percent?
+[ node ]
+  (= "PERCENT" (.ner node)))
+
+
+(defn date?
+[ node ]
+  (= "DATE" (.ner node)))
+
+
+(defn nne-tag
+"Yields the normalized named entity tag annotation, i.e. the 'complete' value of a money, percent, date etc.,
+of which the token for node is part."
+[ node ]
+  (.get node CoreAnnotations$NormalizedNamedEntityTagAnnotation))
+
+
+(defn get-money
+"Yields the full monetary expression node is part of, using its NER attr." 
+[ node ]
+    (if (money? node)
+      (nne-tag node)
+        nil))
 
